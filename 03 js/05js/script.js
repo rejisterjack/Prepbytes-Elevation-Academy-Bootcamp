3b50b4c80849e3845dcddd540099b79614918a9a
Rupam Das <rejisterjack@gmail.com>
2022-04-01 02:20:58 +0530

    day 6

--- 03 js/05js/script.js
+++ 03 js/05js/script.js
@@ -4,6 +4,8 @@
 //   i++
 // }
 // while(i<10)
+
+
 let i = 1;
 while(i<=20){
   console.log(`2*${i} = ${2*i}`);
---
+++ 03 js/06 js/script.js
@@ -0,0 +1,20 @@
+// console.log("working");
+
+// function result(a,b){
+//   // return ((a+b)*100)/200
+//   return ((a+b)/200)*100
+// }
+// console.log(result(80,80));
+
+
+// let arr = [1,2,3,4,5,6,7,8];
+
+// let arr1 = []
+// arr1[0]
+
+// let arr2 = new Array(1,2,3,4)
+
+let arr1=[1,2,3,4,5]
+let arr2=[6,7,8,9,10]
+console.log(arr1.concat(arr2));
+console.log(arr1.join(" "));
