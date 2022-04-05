5a1c55329babc396b415cc61fa70a92e7bf2c52c
Rupam Das <rejisterjack@gmail.com>
2022-04-05 21:25:38 +0530

    done css extra classes

---
+++ 03 js/07 js/script.js
@@ -0,0 +1,8 @@
+let str = "hey there everyone"
+
+console.log(str.startsWith("h"));
+console.log(str.includes("ther"));
+console.log(str.replace("there","now"));
+console.log(str.search("there"));
+console.log(str.indexOf("there"));
+console.log(str.trim());
---
+++ 03 js/08 js/script.js
@@ -0,0 +1,3 @@
+let myMap = new Map();
+myMap.set("name","Ravi");
+myMap.set("age",22);
