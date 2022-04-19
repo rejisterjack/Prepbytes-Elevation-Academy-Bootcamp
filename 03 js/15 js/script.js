707a74ad985a98525c69df6d8144b88b7b8c8827
Rupam Das <rejisterjack@gmail.com>
2022-04-19 18:49:49 +0530

    extra clzss last project

--- 03 js/15 js/script.js
+++ 03 js/15 js/script.js
@@ -1,54 +1,89 @@
-// closures: it is not something that we create manually, instead it is created automatically in centain situations.
+console.log("Working");
 
-const ticketBooking = function () {
-  let passengerCount = 0;
-  return function () {
-    passengerCount++;
-    console.log(`the count of passengers are ${passengerCount}`);
-  }
-}
+// Closures - It is not something that we create manually, instead it is created automatically in certain situations.
 
-const bookie = ticketBooking();  //after line 11, fn ticketBoooking has finished its execution and now cases to exist.
+const ticketBooking = function(){
+    let passengerCount = 0;
 
-bookie();
-bookie();
-bookie();
+    return function(){
+        passengerCount++;
+        console.log(`the count of passengers are ${passengerCount}`);
+    };
+};
 
-// any fn always has access to the variable environment of the execution context in which the function was created.  
+const bookie = ticketBooking();//after line 14, fn ticetBookinh has finished its execution  and now ceases to exist.
 
-// so fn bookie was created in the execution context of ticketBooking and so can access the variables of the functions ticket Booking even though the later fn(ticketBooking) causes to exists
+bookie();//1
+bookie();//2
+bookie();//3
 
-// so even though the execution context of ticket booking ceases still fn Bookie can access the passengerCount variables.
+console.dir(bookie);
+
+// Any fn always has access to the variable environment of the execution context in which the fn was created. 
+// So fn Bookie was created in the execution context of ticketBooking and so fn bookie can access the variables of the function ticketBooking even though the later fn(ticketBooking) ceases to exist.
+
+// So even though the E.C. of ticketBooking ceases still fn Bookie can access the passengerCount Variable which is a part of fn ticketBooking...
 
 let f;
 const g = function(){
-  const a = 23;
-  f=function(){
-    console.log(a*2);
-  }
-}
+    const a = 23;
+
+    f = function(){
+        console.log(a*2);
+    };
+};
 
 const h = function(){
-  const b=25;
-  f=function(){
-    console.log(b*2)
-  }
+    const b = 25;
+
+    f = function(){
+        console.log(b*2);
+    };
+};
+
+g();//
+f();//46
+h();
+f();
+
+console.dir(f);
+
+
+//setTimeout
+
+// setTimeout(function(){
+//     console.log("Hello");
+// },3000)
+
+// setTimeout((drink1, drink2)=>{
+//     console.log(`Here's your drinks ${drink1} and ${drink2} `);
+// },2000,
+// "Virgin Mojito","Blue Lagoon");
+
+
+let drinks = ["Virgin Mojito","Blue Lagoon"];
+let timer = setTimeout((drink1, drink2)=>{
+    console.log(`Here's your drinks ${drink1} and ${drink2} `);
+},2000,
+...drinks//spread operators
+);
+
+if(drinks.includes("Blue Lagoon")){
+    clearTimeout(timer);
 }
 
-g()
-f()
-h()
-f()
-
+//------------------------------------------------------------
+// setTimeout + closures examples
 
 const boardPassengers = (n,wait) =>{
-  let groupOfPeople = n/3;
+    let groupOfPeople = n/3;
 
-  setTimeout(() => {
-      console.log(`The no of passengers are ${n}`);
-      console.log(`The no of passengers per group are ${groupOfPeople}`);
-  }, wait*2000);
-  console.log(`Inboarding all the pasengers in ${wait} seconds`);
+    setTimeout(() => {
+        console.log(`The no of passengers are ${n}`);
+        console.log(`The no of passengers per group are ${groupOfPeople}`);
+    }, wait*1000);
+    console.log(`Onboarding all the pasengers in ${wait} seconds`);
 }
 
+let groupOfPeople = 25;
 boardPassengers(120,3);
