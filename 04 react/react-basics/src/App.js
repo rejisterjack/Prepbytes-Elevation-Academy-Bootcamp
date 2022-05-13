d0ef8b3a7929bccfb3f53d09f99910a9be55533f
Rupam Das <rejisterjack@gmail.com>
2022-05-13 15:28:32 +0530

    update

--- 04 react/react-basics/src/App.js
+++ 04 react/react-basics/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// conponent with normal function syntax
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// conponent name, file name first letter should be in caps
+
+// conponents => javascript logic(business logic) and user interface (jsx)
+// jsx can have only one parent element
+
+const App = ()=>{
+  const name = "rupam";
+  function getName(){
+    console.log(name);
+  }
+  getName();
+  return(
+    <div>
+      <div>Div 1</div>
+      <div>Div 1</div>
     </div>
-  );
+  )
 }
 
 export default App;
