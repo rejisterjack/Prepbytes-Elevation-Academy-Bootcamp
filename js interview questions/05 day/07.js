7a8d29313414ac1b53c52a36ca42b1eee24c9574
Rupam Das <83290469+rejisterjack@users.noreply.github.com>
2022-07-22 14:26:45 +0000

    change name

--- js interview questions/05 day/07.js
+++ js interview questions/05 day/07.js
@@ -1,2 +1,11 @@
-const arr = [0,1,1,0,0,0,0,0,1,1,1,1,1,1]
-console.log(JSON.stringify(arr).match(/[0]/gi).length)
+const arr = [1,2,3,4,5,6,7,8,9]
+let evenNum = 0
+let oddNum = 0
+for(let i of arr){
+	if(i%2===0){
+		evenNum+=1
+	}else{
+		oddNum+=1
+	}
+}
+console.log(`even no: ${evenNum}, odd no: ${oddNum}`)
--- js interview questions/05 day/09.js
+++ js interview questions/05 day/09.js
@@ -1,2 +1,12 @@
-let name = "aeioufhgjkfghjk"
-console.log(name.match(/[aeiou]/gi).length)
+const obj1 = {
+	name: "rupam",
+	about: ["hello", "there", 45]
+}
+const obj2 = {
+	name: "friday",
+	about: ["hello", "there", 45]
+}
+const objectCheck = (obj1, obj2)=>{
+  return JSON.stringify(obj1.about) == JSON.stringify(obj2.about)
+}
+console.log(objectCheck(obj1, obj2))
